2aa1cee157084302397d7fb9666ce0ac4a3c1cfe
Shayan Guha <shayanguha@gmail.com>
2020-03-22 16:11:29 -0700

    Update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ if (!location.hash) {
   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
+const videoId = location.query;
 
 // TODO: Replace with your own channel ID
 const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
@@ -15,6 +16,10 @@ const configuration = {
 };
 let room;
 let pc;
+// RTCDataChannel
+let dataChannel;
+
+let isOfferer = false;
 
 
 function onSuccess() {};
@@ -36,14 +41,17 @@ drone.on('open', error => {
   // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
     console.log('MEMBERS', members);
+    if (members.length > 2) {
+      return alert('The room is full');
+    }
     // If we are the second user to connect to the room we will be creating the offer
-    const isOfferer = members.length === 2;
+    isOfferer = members.length === 2;
     startWebRTC(isOfferer);
   });
 });
 
 // Send signaling data via Scaledrone
-function sendMessage(message) {
+function sendSignalingMessage(message) {
   drone.publish({
     room: roomName,
     message
@@ -57,7 +65,7 @@ function startWebRTC(isOfferer) {
   // message to the other peer through the signaling server
   pc.onicecandidate = event => {
     if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
+      sendSignalingMessage({'candidate': event.candidate});
     }
   };
 
@@ -65,6 +73,14 @@ function startWebRTC(isOfferer) {
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
       pc.createOffer().then(localDescCreated).catch(onError);
+    }
+    dataChannel = pc.createDataChannel('videosync');
+    setupDataChannel();
+  }  else {
+    // If user is not the offerer let wait for a data channel
+    pc.ondatachannel = event => {
+      dataChannel = event.channel;
+      setupDataChannel();
     }
   }
 
@@ -113,7 +129,107 @@ function startWebRTC(isOfferer) {
 function localDescCreated(desc) {
   pc.setLocalDescription(
     desc,
-    () => sendMessage({'sdp': pc.localDescription}),
+    () => sendSignalingMessage({'sdp': pc.localDescription}),
     onError
   );
 }
+
+function processMessage(event) {
+  console.log("processMessage", event);
+
+  switch (event.data) {
+    case YT.PlayerState.PLAYING:
+      // window.phone.sendData(event);
+      player.playVideo();
+      break;
+    case YT.PlayerState.PAUSED:
+      player.pauseVideo()
+      // window.phone.sendData(event);
+      break;
+    case YT.PlayerState.BUFFERING: // If they seeked, dont send this.
+      player.seekTo(event.target.playerInfo.currentTime);
+      // else window.phone.sendData(event);
+  }
+}
+
+// Hook up data channel event handlers
+function setupDataChannel() {
+  console.log("setupDataChannel");
+  checkDataChannelState();
+  dataChannel.onopen = checkDataChannelState;
+  dataChannel.onclose = checkDataChannelState;
+  dataChannel.onmessage = event =>
+    processMessage(JSON.parse(event.data))
+}
+
+function checkDataChannelState() {
+  console.log('WebRTC channel state is:', dataChannel.readyState);
+}
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  // event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+// function onPlayerStateChange(event) {
+//   console.log("EVENT", event);
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     setTimeout(stopVideo, 6000);
+//     done = true;
+//   }
+// }
+
+function onPlayerStateChange(event) {
+  if (!isOfferer) {
+    console.log("send", JSON.stringify(event))
+    dataChannel.send(JSON.stringify(event));
+  }
+  console.log("EVENT", isOfferer, event.data, event.target.playerInfo.currentTime);
+  if (!window.phone) return; // No active video chat, return.
+  event.username = user_name;
+  switch (event.data) {
+    case YT.PlayerState.PLAYING:
+      if (done) return;
+      // window.phone.sendData(event);
+      break;
+    case YT.PlayerState.PAUSED:
+      // window.phone.sendData(event);
+      break;
+    case YT.PlayerState.BUFFERING: // If they seeked, dont send this.
+      if (seek) seek = false;
+      // else window.phone.sendData(event);
+  }
+}
+
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+
